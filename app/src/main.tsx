8ed5d64f5554904c01af624b584112125170187f
MrYious <rosariomark37@gmail.com>
2024-02-20 22:33:57 +0800

    setup router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,11 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ app/src/routes/Home.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home () {
+    return (
+        <main className=" flex min-h-screen justify-center items-center">
+            Hello this is the game!
+        </main>
+    )
+}
