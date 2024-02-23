895e3de97a7f083609fd8ad6a7a65ebdb444a82b
MrYious <rosariomark37@gmail.com>
2024-02-23 16:42:43 +0800

    extract menu bar to new component and fix initial data loading

---
+++ app/src/components/MenuBar.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom"
+
+export const MenuBar = (props: {handleCloseMenu: () => void}) => {
+
+    const navigate = useNavigate()
+
+    const handleNewGame = () => {
+        navigate('/menu', {replace: true})
+    }
+
+    const handleQuitGame = () => {
+        navigate('/', {replace: true})
+    }
+
+    return(<section className="absolute flex items-center justify-center w-full min-h-screen bg-gray-900 bg-opacity-50">
+        <div className="relative flex flex-col items-center justify-center w-1/3 gap-5 py-10 bg-blue-700 border-t-8 border-blue-500 select-none bg-opacity-80 border-x-4 rounded-3xl">
+            <div className="text-white text-7xl">
+                Paused
+            </div>
+            <button
+                onClick={props.handleCloseMenu}
+                className="py-2 text-4xl font-bold tracking-wide text-center text-white bg-blue-700 border-t-2 border-b-4 border-blue-300 rounded-full select-none hover:bg-blue-800 w-60"
+            >
+                CONTINUE
+            </button>
+            <button
+                onClick={handleNewGame}
+                className="py-2 text-4xl font-bold tracking-wide text-center text-white bg-blue-700 border-t-2 border-b-4 border-blue-300 rounded-full select-none hover:bg-blue-800 w-60"
+            >
+                NEW GAME
+            </button>
+            <button
+                onClick={handleQuitGame}
+                className="py-2 text-4xl font-bold tracking-wide text-center text-white border-t-2 border-b-4 border-blue-300 rounded-full select-none bg-violet-700 hover:bg-violet-800 w-60"
+            >
+                QUIT GAME
+            </button>
+        </div>
+    </section>)
+}
--- app/src/routes/Play.tsx
+++ app/src/routes/Play.tsx
@@ -1,75 +1,57 @@
 import { Navigate, useNavigate } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../hooks/useReduxHooks";
+import { useEffect, useState } from "react";
 
 import { FaHeart } from "react-icons/fa";
+import { MenuBar } from "../components/MenuBar";
 import ProgressBar from "@ramonak/react-progress-bar";
+import { loadGameData } from "../slicers/gameDataSlicer";
 import menu from "../assets/images/icon-menu.svg"
-import { useAppSelector } from "../hooks/useReduxHooks";
-import { useState } from "react";
 
 export default function Play () {
-    const navigate = useNavigate()
-    const state = useAppSelector(state => state.gameData)
-    console.log(state);
+    console.log('Render');
 
-    if (state.status === 'Inactive') {
+    const dispatch = useAppDispatch()
+    const state = useAppSelector(state => state.gameData)
+
+    const sessionData = sessionStorage.getItem('gameData')
+
+    if (state.status === 'Inactive' && !sessionData) {
         return <Navigate to="/" replace={true} />
-    } else if (state.status === 'Active') {
-        sessionStorage.setItem('savedData', JSON.stringify(state))
     }
+
+    useEffect(() => {
+        console.log('Initial State', state);
+        if (sessionData) {
+            const gameData = sessionStorage.getItem('gameData')
+            const parsedGameData = JSON.parse(gameData ? gameData : '')
+            console.log('Parsed Game Data', parsedGameData);
+            dispatch(loadGameData(parsedGameData))
+        } else {
+            sessionStorage.setItem('gameData', JSON.stringify(state))
+        }
+    }, [])
 
     const [isMenu, setIsMenu] = useState(false)
 
     const handleOpenMenu = () => {
         setIsMenu(true)
     }
-
-    const handleContinue = () => {
+    const handleCloseMenu = () => {
         setIsMenu(false)
-    }
-
-    const handleNewGame = () => {
-        navigate('/menu', {replace: true})
-    }
-
-    const handleQuitGame = () => {
-        navigate('/', {replace: true})
     }
 
     return(<main className="flex flex-col min-h-screen bg-center bg-no-repeat bg-cover bg-bg-desktop">
         {
             isMenu &&
-            <section className="absolute flex items-center justify-center w-full min-h-screen bg-gray-900 bg-opacity-50">
-                <div className="relative flex flex-col items-center justify-center w-1/3 gap-5 py-10 bg-blue-700 border-t-8 border-blue-500 select-none bg-opacity-80 border-x-4 rounded-3xl">
-                    <div className="text-white text-7xl">
-                        Paused
-                    </div>
-                    <button
-                        onClick={handleContinue}
-                        className="py-2 text-4xl font-bold tracking-wide text-center text-white bg-blue-700 border-t-2 border-b-4 border-blue-300 rounded-full select-none hover:bg-blue-800 w-60"
-                    >
-                        CONTINUE
-                    </button>
-                    <button
-                        onClick={handleNewGame}
-                        className="py-2 text-4xl font-bold tracking-wide text-center text-white bg-blue-700 border-t-2 border-b-4 border-blue-300 rounded-full select-none hover:bg-blue-800 w-60"
-                    >
-                        NEW GAME
-                    </button>
-                    <button
-                        onClick={handleQuitGame}
-                        className="py-2 text-4xl font-bold tracking-wide text-center text-white border-t-2 border-b-4 border-blue-300 rounded-full select-none bg-violet-700 hover:bg-violet-800 w-60"
-                    >
-                        QUIT GAME
-                    </button>
-                </div>
-            </section>
+            <MenuBar handleCloseMenu={handleCloseMenu} />
         }
         <section className="flex items-center gap-10 px-10 py-5 border-2 border-black">
             <button onClick={handleOpenMenu} className="p-5 border-b-2 rounded-full select-none border-violet-700 bg-violet-600 hover:bg-violet-800">
                 <img src={menu} alt="menu" className="w-[32px]" />
             </button>
             <h1 className="font-bold text-blue-100 select-none text-7xl grow">
-                Animals
+                {state.category}
             </h1>
             <div className="flex items-center gap-5">
                 <ProgressBar completed={33} maxCompleted={100} className="bg-white border-8 border-white rounded-full select-none w-60" baseBgColor="#d1d5db" bgColor="#dc2626" customLabel=" " />
