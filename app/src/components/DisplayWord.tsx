2384d837b94316dd1b511f7e0bb40f919a720dfa
MrYious <rosariomark37@gmail.com>
2024-02-24 19:47:58 +0800

    add key

--- app/src/components/DisplayWord.tsx
+++ app/src/components/DisplayWord.tsx
@@ -6,8 +6,8 @@ export const DisplayWord = () => {
 
     return(<>
         {
-            state.word.selectedWord.split('').map(letter =>
-                <div className={`${state.word.correctLetters.includes(letter) ? 'border-gray-200' : 'border-white opacity-30'} text-7xl px-8 py-5 bg-blue-700 rounded-3xl text-white border-4 border-t-2 `}>
+            state.word.selectedWord.split('').map((letter,i) =>
+                <div key={i} className={`${state.word.correctLetters.includes(letter) ? 'border-gray-200' : 'border-white opacity-30'} text-7xl px-8 py-5 bg-blue-700 rounded-3xl text-white border-4 border-t-2 `}>
                     {state.word.correctLetters.includes(letter) ? letter : '-'}
                 </div>
             )
