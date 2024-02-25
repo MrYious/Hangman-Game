e7520279d562dfb764aa56703ef5fefcfacf8f63
MrYious <rosariomark37@gmail.com>
2024-02-25 18:56:03 +0800

    remove log

--- app/src/components/Keyboard.tsx
+++ app/src/components/Keyboard.tsx
@@ -4,7 +4,6 @@ import { useAppDispatch, useAppSelector } from "../hooks/useReduxHooks"
 export const Keyboard = () => {
     const dispatch = useAppDispatch()
     const state = useAppSelector(state => state.gameData.word)
-    console.log(state);
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     const handleAttempt = (letter: string) => {
