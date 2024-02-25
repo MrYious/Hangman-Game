4a77ced0ecd9453e6d2338910ac14fa87e86eca9
MrYious <rosariomark37@gmail.com>
2024-02-25 21:35:29 +0800

    fix style

--- app/src/components/DisplayWord.tsx
+++ app/src/components/DisplayWord.tsx
@@ -7,7 +7,7 @@ export const DisplayWord = () => {
     return(<>
         {
             state.word.selectedWord.split('').map((letter,i) =>
-                <div key={i} className={`${state.word.correctLetters.includes(letter) ? 'border-gray-200' : 'border-white opacity-30'} ${letter === ' ' ? ' opacity-0' : 'px-8 '} text-7xl bg-blue-700 py-5 rounded-3xl text-white border-2 border-t-[1px] `}>
+                <div key={i} className={`${state.word.correctLetters.includes(letter) ? 'border-gray-200' : letter === ' ' ? ' opacity-0 ' : 'px-8 border-white opacity-30'} text-7xl bg-blue-700 py-5 rounded-3xl text-white border-2 border-t-[1px] `}>
                     {state.word.correctLetters.includes(letter) ? letter : '-'}
                 </div>
             )
